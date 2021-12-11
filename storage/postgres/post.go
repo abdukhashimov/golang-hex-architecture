16e3f24f6486dc7ea5accd9b68f727269d53206e
Madiyor <abdukhashimov@yandex.ru>
2021-12-11 15:31:14 +0500

    fix: add storage methods

--- storage/postgres/post.go
+++ storage/postgres/post.go
@@ -1,1 +1,23 @@
 package postgres
+
+import (
+	"github.com/abdukhashimov/golang-hex-architecture/service/models"
+	"github.com/abdukhashimov/golang-hex-architecture/storage/repo"
+	"github.com/jmoiron/sqlx"
+)
+
+type posts struct {
+	db *sqlx.DB
+}
+
+func NewPostStorage(db *sqlx.DB) repo.PostI {
+	return &posts{db: db}
+}
+
+func (p *posts) Create(payload models.Post) (string, error) {
+	return "", nil
+}
+
+func (p *posts) GetUserPosts(filter models.PostFilter) (models.PostAll, error) {
+	return models.PostAll{}, nil
+}
--- storage/repo/post.go
+++ storage/repo/post.go
@@ -1,1 +1,8 @@
 package repo
+
+import "github.com/abdukhashimov/golang-hex-architecture/service/models"
+
+type PostI interface {
+	Create(payload models.Post) (string, error)
+	GetUserPosts(filter models.PostFilter) (models.PostAll, error)
+}
