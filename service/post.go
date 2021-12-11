68c675e5196559e9482e2a6c783beecce2bf97a5
Madiyor <abdukhashimov@yandex.ru>
2021-12-11 15:35:52 +0500

    fix: add service

--- service/post.go
+++ service/post.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/abdukhashimov/golang-hex-architecture/service/models"
+	"github.com/abdukhashimov/golang-hex-architecture/storage"
 )
 
 type PostI interface {
@@ -12,16 +13,25 @@ type PostI interface {
 }
 
 type post struct {
+	db storage.StorageI
 }
 
-func NewPostService() PostI {
-	return &post{}
+func NewPostService(db storage.StorageI) PostI {
+	return &post{
+		db: db,
+	}
 }
 
-func (p *post) CreatePost(context.Context, models.Post) (string, error) {
-	return "", nil
+func (p *post) CreatePost(ctx context.Context, req models.Post) (string, error) {
+	res, err := p.db.Post().Create(req)
+
+	if err != nil {
+		return "", err
+	}
+
+	return res, nil
 }
 
-func (p *post) GetPostsByAuthhor(context.Context, models.PostFilter) (models.PostAll, error) {
+func (p *post) GetPostsByAuthhor(ctx context.Context, filter models.PostFilter) (models.PostAll, error) {
 	return models.PostAll{}, nil
 }
