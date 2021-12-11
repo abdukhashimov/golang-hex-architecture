d451b42e1aa2d108d0c81657639145bb89781c5d
Madiyor <abdukhashimov@yandex.ru>
2021-12-11 13:38:49 +0500

    fix: add custom logger function

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,9 @@ package main
 
 import (
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 func main() {
-	var cfg zap.Config = zap.NewProductionConfig()
-	cfg.EncoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
 
-	logger, err := cfg.Build()
-	if err != nil {
-		panic(err)
-	}
-	defer logger.Sync()
 	logger.Info("Hello world", zap.String("hello", "hello"))
 }
