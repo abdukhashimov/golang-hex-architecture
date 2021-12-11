7f9feefb5d3f88102454ddda53de86793b7c7878
Madiyor <abdukhashimov@yandex.ru>
2021-12-11 13:39:45 +0500

    fix: add new logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"github.com/abdukhashimov/golang-hex-architecture/pkg/logger"
 	"go.uber.org/zap"
 )
 
 func main() {
-
+	logger := logger.NewLogger()
 	logger.Info("Hello world", zap.String("hello", "hello"))
 }
