474fb6af38c08600abb483bca53aced786dde974
Madiyor <abdukhashimov@yandex.ru>
2021-12-11 15:24:19 +0500

    fix: implement functions

--- service/post.go
+++ service/post.go
@@ -10,3 +10,18 @@ type PostI interface {
 	CreatePost(context.Context, models.Post) (string, error)
 	GetPostsByAuthhor(context.Context, models.PostFilter) (models.PostAll, error)
 }
+
+type post struct {
+}
+
+func NewPostService() PostI {
+	return &post{}
+}
+
+func (p *post) CreatePost(context.Context, models.Post) (string, error) {
+	return "", nil
+}
+
+func (p *post) GetPostsByAuthhor(context.Context, models.PostFilter) (models.PostAll, error) {
+	return models.PostAll{}, nil
+}
