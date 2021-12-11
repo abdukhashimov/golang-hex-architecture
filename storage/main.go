cc1ede618dd162642d856852cd7dc4cf5ef820b5
Madiyor <abdukhashimov@yandex.ru>
2021-12-11 15:33:05 +0500

    fix: add post storage

--- storage/main.go
+++ storage/main.go
@@ -1,1 +1,25 @@
 package storage
+
+import (
+	"github.com/abdukhashimov/golang-hex-architecture/storage/postgres"
+	"github.com/abdukhashimov/golang-hex-architecture/storage/repo"
+	"github.com/jmoiron/sqlx"
+)
+
+type storage struct {
+	postStorage repo.PostI
+}
+
+type StorageI interface {
+	Post() repo.PostI
+}
+
+func NewStorage(db *sqlx.DB) StorageI {
+	return &storage{
+		postStorage: postgres.NewPostStorage(db),
+	}
+}
+
+func (s *storage) Post() repo.PostI {
+	return s.postStorage
+}
